0d42440e78b70d20bcbef17547fbd3ceed63354f
6QHTSK <psk2019@qq.com>
2020-07-10 00:45:56 +0800

    更新一下bestdori 分析

--- components/bestdoriany/script.js
+++ components/bestdoriany/script.js
@@ -1,7 +1,7 @@
 function returnleveldata(leveldata){
     var rtr=[];
-    for(i=5;i<=30;i++){
-        rtr.push(leveldata[i])
+    for(i=0;i<=25;i++){
+        rtr.push(leveldata[i][1])
     }
     return rtr
 }
@@ -20,30 +20,31 @@ function calcchartdata(vm) {
     var diffctx = document.getElementById("diffchart").getContext("2d")
     var levelctx = document.getElementById("levelchart").getContext("2d")
     var timectx = document.getElementById("timechart").getContext("2d")
+    var likeperchartctx = document.getElementById("likeperchart").getContext("2d")
     var diffdata = [
         {
             label: "Easy",
-            value: vm.results["diffcounter"]["0"],
+            value: vm.results["diffcounter"][0][1],
             color: "#409EFF"
         },
         {
             label: "Normal",
-            value: vm.results["diffcounter"]["1"],
+            value: vm.results["diffcounter"][1][1],
             color: "#67C23A"
         },
         {
             label: "Hard",
-            value: vm.results["diffcounter"]["2"],
+            value: vm.results["diffcounter"][2][1],
             color: "#E6A23C"
         },
         {
             label: "Expert",
-            value: vm.results["diffcounter"]["3"],
+            value: vm.results["diffcounter"][3][1],
             color: "#F56C6C"
         },
         {
             label: "Special",
-            value: vm.results["diffcounter"]["4"],
+            value: vm.results["diffcounter"][4][1],
             color: "#EC41D5"
         }
     ];
@@ -51,7 +52,7 @@ function calcchartdata(vm) {
         type: 'pie',
         data: {
             datasets: [{
-                data: [vm.results["diffcounter"]["0"], vm.results["diffcounter"]["1"], vm.results["diffcounter"]["2"], vm.results["diffcounter"]["3"], vm.results["diffcounter"]["4"]],
+                data: [vm.results["diffcounter"][0][1], vm.results["diffcounter"][1][1], vm.results["diffcounter"][2][1], vm.results["diffcounter"][3][1], vm.results["diffcounter"][4][1]],
                 backgroundColor: ["#409EFF", "#67C23A", "#E6A23C", "#F56C6C", "#EC41D5"]
             }
             ],
@@ -111,7 +112,24 @@ function calcchartdata(vm) {
             },
             title: {
                 display: true,
-                text: "谱面发表时间分布"
+                text: "有效谱面发表时间分布"
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    }
+    var likeperconfig = {
+        type: 'line',
+        data : returntimedata(vm.results["likeperchart"]),
+        options: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: "有效谱面平均获赞"
             },
             animation: {
                 animateScale: true,
@@ -124,12 +142,17 @@ function calcchartdata(vm) {
     vm.diffdata = diffdata
     var levelchart = new Chart(levelctx,levelconfig)
     var timechart = new Chart(timectx,timeconfig)
+    var likeperchart = new Chart(likeperchartctx,likeperconfig)
 }
 module.exports = {
     data: function () {
         return {
             onloading: false,
+            searchload: false,
+            searchsee : false,
+            searchuser : "",
             results: {},
+            searchres: {},
             diffdata : [],
             levelconfig : {data:{}}
         }
@@ -141,13 +164,13 @@ module.exports = {
         pullbdchart() {
             this.onloading = true
             vm = this
-            url = "https://bird.ioliu.cn/v1?url=http://106.55.249.77:62001/calcdata";
+            url = "https://bird.ioliu.cn/v1?url=http://106.55.249.77:7091/calcdata";
             axios.get(url).then(function (res) {
                 vm.onloading = false;
                 if (res.status == 200) {
                     vm.results = res.data
                     calcchartdata(vm)
-                    console.log(res.data)
+                    //console.log(res.data)
                 }
                 else {
                     vm.$message("拉取资源失败")
@@ -155,8 +178,54 @@ module.exports = {
             }
             )
         },
+        querySearch(queryString, cb) {
+            rtr = queryString ? this.createFilter(queryString) : user;
+            console.log(rtr)
+            // 调用 callback 返回建议列表的数据
+            cb(rtr);
+        },
+        createFilter(queryString) {
+            rtr = []
+            //console.log(this.results["usernames"])
+            for (i = 0; i < this.results["usernames"].length; i ++ ){
+                name = this.results["usernames"][i][0]
+                value = this.results["usernames"][i][1] != null ? this.results["usernames"][i][1] + '@' + this.results["usernames"][i][0] : this.results["usernames"][i][0]
+                //console.log(this.results["usernames"][i][0])
+              if (value.toLowerCase().indexOf(queryString.toLowerCase()) != -1){
+                  
+                  rtr.push({"value":value,"username":name})
+              }
+            }
+            return rtr
+        },
+        handleSelect(item) {
+            this.search(item.username)
+            console.log(item)
+        },
+        search(username){
+            this.searchload = true
+            vm = this
+            url = "https://bird.ioliu.cn/v1?url=http://106.55.249.77:7091/calcauthor?author=" + username
+            axios.get(url).then(function (res) {
+                vm.searchload = false
+                if (res.status == 200) {
+                    vm.searchres = res.data
+                    if(res.data["result"]){
+                        console.log(res.data)
+                        vm.searchsee = true
+                    }
+                    else{
+                        vm.$message("无此谱师或此谱师未满足条件")
+                    }
+                }
+                else {
+                    vm.$message("拉取资源失败")
+                }
+            })
+        }
     },
     mounted() {
         this.pullbdchart()
+        //this.search("psk2019")
     }
 }
