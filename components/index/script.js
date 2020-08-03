e40ac3aec0be11420ac51bf1c3ac7c783a3ebc99
稳音绫与6QHTSK <psk2019@qq.com>
2020-08-03 16:15:52 +0800

    永久跳转

--- components/index/script.js
+++ components/index/script.js
@@ -6,10 +6,10 @@ module.export={
         }
     },
     mounted(){
-        var sUserAgent = navigator.userAgent;
+        //var sUserAgent = navigator.userAgent;
         //print(sUserAgent)
-        if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
-            location.href = 'http://coppercomplex.gitee.io/mobile/index.html';
-        }
+        //if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
+        //    location.href = 'http://coppercomplex.gitee.io/mobile/index.html';
+        //}
     }
 }
